444df5eaffbea1821ea72d39d4607f5ab81a33e8
Finanflix <projectfinanflix@gmail.com>
2025-04-29 13:24:54 -0300

    changesssss

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,49 +1,49 @@
+import { useContext, useEffect, useState } from "react";
 import { BackgroundMouse } from "@/components/BackgroundMouse/BackgroundMouse";
 import BotpressChat from "@/components/BotpressChat/BotpressChat";
 import { HeroInnovative } from "@/components/HeroSection/HeroSection";
 import SplineSceneBasic from "@/components/HeroSection/HeroSection2";
 import { I18nContext } from "@/utils/i18nProvider";
-import { useContext, useEffect, useState } from "react";
+import { LoadingScreen } from "@/components/LoadingScreen/LoadingScreen";
 
 export const Home = () => {
-  // const [isLoading, setIsLoading] = useState(true);
   const [isLowPerformance, setIsLowPerformance] = useState(false);
+  const [isCheckingPerformance, setIsCheckingPerformance] = useState(true);
   const context = useContext(I18nContext);
 
   useEffect(() => {
     let lastTime = performance.now();
-
-    let fps;
     let frames = 0;
-    let timeCounter = 0;
+    let totalElapsed = 0;
+    let rafId: number;
 
-    const checkPerformance = (time: number) => {
+    const measurePerformance = (time: number) => {
       frames++;
-      timeCounter += time - lastTime;
+      const delta = time - lastTime;
+      totalElapsed += delta;
       lastTime = time;
 
-      if (timeCounter >= 1000) {
-        // cada 1 segundo
-        fps = frames;
-        frames = 0;
-        timeCounter = 0;
-        console.log("fps", fps);
+      if (totalElapsed < 3000) {
+        rafId = requestAnimationFrame(measurePerformance);
+      } else {
+        const avgFps = frames / (totalElapsed / 1000);
+        console.log("Average FPS:", avgFps);
 
-        if (fps < 30) {
-          // Si el usuario tiene menos de 20fps
+        if (avgFps < 30) {
           setIsLowPerformance(true);
         }
-      }
-
-      if (!isLowPerformance) {
-        requestAnimationFrame(checkPerformance);
+        setIsCheckingPerformance(false);
       }
     };
 
-    const id = requestAnimationFrame(checkPerformance);
+    rafId = requestAnimationFrame(measurePerformance);
 
-    return () => cancelAnimationFrame(id);
-  }, [isLowPerformance]);
+    return () => cancelAnimationFrame(rafId);
+  }, []);
+
+  if (isCheckingPerformance) {
+    return <LoadingScreen />;
+  }
 
   if (isLowPerformance) {
     return (
@@ -57,6 +57,7 @@ export const Home = () => {
       </BackgroundMouse>
     );
   }
+
   return (
     <BackgroundMouse>
       <SplineSceneBasic />
